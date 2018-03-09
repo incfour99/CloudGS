55276e49721b4f3ab0a3dd55f59166e51ebb6b9a
declear <kim0375@naver.com>
2018-03-09 09:44:08 +0900

    미국, 일본 PC버전 접속을 위한 bat 파일 추가

---
+++ GamingAnyWhere/gaminganywhere-0.8.0-bin.win32/bin/ga-client-jp.bat
@@ -0,0 +1,3 @@
+ga-client.exe config/client.cloudgs.abs.conf rtsp://176.32.72.169:8554/desktop
+
+pause
---
+++ GamingAnyWhere/gaminganywhere-0.8.0-bin.win32/bin/ga-client-us.bat
@@ -0,0 +1,3 @@
+ga-client.exe config/client.cloudgs.abs.conf rtsp://34.235.58.175:8554/desktop
+
+pause
